8fa74c75fea558565eebfc1f5e355c25d929a4f7
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-09-20 11:43:24 -0700

    created the contact me section

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,12 +1,24 @@
+import styles from './contact.module.css'
+
 export default function Contact() {
     return(
-        <div>
-            <h2 className={`big-font-2 section-margin`} >
+        <div className={`section-margin`}>
+            <h2 className={`big-font`} >
                 Contact Me
             </h2>
-            <form action="">
-                <input type="text" />
-                <input type="email" />
+            <form action="" className={`${styles.form} flex-vert`}>
+                <div>
+                    <p>Name</p>
+                    <input type="text" placeholder='John Doe' />
+                </div>
+                <div>
+                    <p>Email</p>
+                    <input type="email" placeholder='example@gmail.com' />
+                </div>
+                <div>
+                    <p>Message</p>
+                    <textarea placeholder='Enter your message here' />
+                </div>
             </form>
         </div>
     )
